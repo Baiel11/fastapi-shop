a0cb8ed37618ec5a2a15d481e58ddfec95e8c0da
Neon <egemberd1ev.xx@gmail.com>
2026-04-25 22:52:43 +0600

    fix router

--- backend/app/routes/products.py
+++ backend/app/routes/products.py
@@ -9,17 +9,17 @@ router = APIRouter(
     tags=['products']
 )
 
-@router("", response_model=list[ProductResponse], status_code=status.HTTP_200_OK)
+@router.get("", response_model=ProductListResponse, status_code=status.HTTP_200_OK)
 def get_products(db: Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_all_products()
 
-@router("/{product_id}", response_model=list[ProductResponse], status_code=status.HTTP_200_OK)
+@router.get("/{product_id}", response_model=ProductResponse, status_code=status.HTTP_200_OK)
 def get_product(product_id: int, db: Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_product_by_id(product_id)
 
-@router("/category/{category_id}", response_model=ProductListResponse, status_code=status.HTTP_200_OK)
+@router.get("/category/{category_id}", response_model=ProductListResponse, status_code=status.HTTP_200_OK)
 def get_products_by_category(category_id: int, db: Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_products_by_category(category_id)
