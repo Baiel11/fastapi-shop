e8185c70fa2f9a7f739ae7cfea7893c98ab15a3e
Neon <egemberd1ev.xx@gmail.com>
2026-07-17 18:31:47 +0600

    add admin schema exports

---
+++ backend/app/schemas/admin/__init__.py
@@ -0,0 +1,9 @@
+from .dashboard import DashboardStats
+from .user_admin import AdminUserResponse, UpdateUserStatus, UpdateUserRole
+
+__all__ = [
+    "DashboardStats",
+    "AdminUserResponse",
+    "UpdateUserStatus",
+    "UpdateUserRole",
+]
