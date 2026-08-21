a090966ac4c7a5ead1992435287589afec1f97ee
Neon <egemberd1ev.xx@gmail.com>
2026-08-21 20:01:44 +0600

    add jti id

--- backend/app/core/security/jwt.py
+++ backend/app/core/security/jwt.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta, timezone
+from uuid import uuid4
 from jose import jwt, JWTError
 from ..config import settings
 
+
 TOKEN_TYPE_ACCESS = "access"
 TOKEN_TYPE_REFRESH = "refresh"
+
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
@@ -12,7 +15,9 @@ def create_access_token(data: dict) -> str:
     )
     to_encode["exp"] = expire
     to_encode["type"] = TOKEN_TYPE_ACCESS
+    to_encode["jti"] = str(uuid4())
     return jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+
 
 def create_refresh_token(data: dict) -> str:
     to_encode = data.copy()
@@ -21,7 +26,9 @@ def create_refresh_token(data: dict) -> str:
     )
     to_encode["exp"] = expire
     to_encode["type"] = TOKEN_TYPE_REFRESH
+    to_encode["jti"] = str(uuid4())
     return jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+
 
 def decode_token(token: str) -> dict:
     return jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
