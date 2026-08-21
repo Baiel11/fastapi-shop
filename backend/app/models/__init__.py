02fee5fcc3a13d7feef159f78935c52b962ba8f8
Neon <egemberd1ev.xx@gmail.com>
2026-08-21 20:00:16 +0600

    register refresh token

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from .category import Category
 from .product import Product
 from .user import User
 from .cart_item import CartItem
+from .refresh_token import RefreshToken
 
-__all__ = [Category, Product, User, CartItem]
+__all__ = [Category, Product, User, CartItem, RefreshToken]
