448d96609aaa7d6c05e2fc4491b1ee8165642807
Neon <egemberd1ev.xx@gmail.com>
2026-08-22 21:25:46 +0600

    export session and password customer routes

--- backend/app/routes/customer/__init__.py
+++ backend/app/routes/customer/__init__.py
@@ -2,10 +2,14 @@ from .products import router as products_router
 from .categories import router as categories_router
 from .cart import router as cart_router
 from .auth import router as auth_router
+from .session import router as session_router
+from .password import router as password_router
 
 __all__ = [
     "products_router",
     "categories_router",
     "cart_router",
     "auth_router",
+    "session_router",
+    "password_router",
 ]
